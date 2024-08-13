52ec9a5573255ccbcd29d353d326798a36ccead5
Serhii Ddobrovolskyi <serhii.dobrovolskyi@dipdev.studio>
2024-08-13 17:36:19 +0300

    Fix: random color generator

--- lib/core/util/color_utils.dart
+++ lib/core/util/color_utils.dart
@@ -3,12 +3,12 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class ColorUtils {
-  static Color generatePastelColor() {
+  static Color generateVibrantColor() {
     final random = Random();
 
-    int r = 200 + random.nextInt(56);
-    int g = 200 + random.nextInt(56);
-    int b = 200 + random.nextInt(56);
+    int r = 200 + random.nextInt(101);
+    int g = 200 + random.nextInt(101);
+    int b = 200 + random.nextInt(101);
 
     return Color.fromRGBO(r, g, b, 1);
   }
